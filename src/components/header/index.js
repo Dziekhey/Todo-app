98511f53b8df466def3a836e609d3b22508eb08f
Dziekhey <dakeysandra@gmail>
2024-02-15 11:20:34 +0000

    Set up the project

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <section>
+            <h1>TODO</h1>
+        </section>
+    );
+}
+
+export default Header;
---
+++ src/components/todo-list/index.js
@@ -0,0 +1,14 @@
+function TodoList () {
+    return (
+        <ul>
+            <li>Complete online Javascript course</li>
+            <li>Jog around the park 3 times</li>
+            <li>10 minutes meditation</li>
+            <li>Read for 1 hour</li>
+            <li>Pick up groceries</li>
+            <li>Complete CSS course</li>
+        </ul>
+    );
+}
+
+export default TodoList;
