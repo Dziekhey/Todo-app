fd03865c89c2c228556a07413d6d2b8529c06169
Dziekhey <dakeysandra@gmail>
2024-03-20 09:38:37 +0000

    19/03/2024 update

--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,47 +1,56 @@
-import {useState, useEffect} from "react";
-import {useLocalStorage} from 'usehooks-ts';
+import { useState, useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
 import TodoItem from "../todo-item";
 
-
 function TodoList() {
-    // let todo;
-    const [todos, setTodos] = useState([]);
+  // let todo;
+  const [todos, setTodos] = useState([]);
 
-    const getTodos = async () => {
-        // Get todos from todo-api
-        const response = await fetch('http://localhost:4000/todos')
-        const data = await response.json();
-        console.log(data)
-        // Update todos state
-    }
+  const getTodos = async () => {
+    // Get todos from todo-api
+    const response = await fetch("http://localhost:4000/todos");
+    const data = await response.json();
+    console.log(data);
+    // Update todos state
+    setTodos(data);
+  };
 
-    function deleteAll () {
-        setTodos([]);
-    }
+  async function deleteAll() {
+    // Delete todos from todo-api
+    const response = await fetch("http://localhost:4000/todos", {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    // Update todos state
+    setTodos([]);
+  }
 
-    useEffect(() => {
-        getTodos()
-    }, [])
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-    // function getTodos() {
-    //     // Get all todos from local storage and store it.
-    //     let todos = JSON.parse(localStorage.getItem('TODO_KEY')) || [];
-    //     // Update react state
-    //     setTodos(todos);
-    // }
+  // function getTodos() {
+  //     // Get all todos from local storage and store it.
+  //     let todos = JSON.parse(localStorage.getItem('TODO_KEY')) || [];
+  //     // Update react state
+  //     setTodos(todos);
+  // }
 
-    // useEffect(getTodos, []);
+  // useEffect(getTodos, []);
 
-    return (
-        <section>
-            <button onClick={deleteAll} className="btn btn-danger my-4 ms-2">Delete All</button>
-        <ul className="list-group">
-            {todos.map(function(todo, index){
-             return  <TodoItem todo={todo} index={index} />
-            })}
-        </ul>
-        </section>
-    );
+  return (
+    <section>
+      <button onClick={deleteAll} className="btn btn-danger my-4 ms-2">
+        Delete All
+      </button>
+      <ul className="list-group">
+        {todos.map(function (todo, index) {
+          return <TodoItem key={todo._id} todo={todo.title} index={index} />;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default TodoList;
